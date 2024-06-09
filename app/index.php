54c6073ee36c01dd12aaed38ad7f91b5acd37071
Matias Skenen <93952537+matiastaoskn@users.noreply.github.com>
2024-06-09 12:43:22 -0300

    valido mesas, producto y usuarios

--- app/index.php
+++ app/index.php
@@ -32,6 +32,11 @@ $app->post("/usuarios", function(Request $request, Response $response, $args)
     {
         $db = conectar();
 
+        if (usuarioExiste($params['usuario'], $db)) {
+            $response->getBody()->write(json_encode(["error" => "El usuario ya existe en la base de datos"]));
+            return $response->withHeader('Content-Type', 'application/json');
+        }
+
         $consulta = "INSERT INTO usuarios (usuario, tipo, nombre, apellido) 
         VALUES (:usuario, :tipo, :nombre, :apellido)";
     
@@ -57,17 +62,86 @@ $app->post("/usuarios", function(Request $request, Response $response, $args)
     }
 });
 
+$app->post("/productos", function ($request, $response, array $args) {
 
+    $params = $request->getParsedBody();
 
-/*
-$app->get("/usuarios", function(Request $request, Response $response, $args)
-{
-    $params = $request->getQueryParams();
-    
-    $response->getBody()->write(json_encode($params)); 
-    return $response;
+    if (!isset($params["tipo"]) || empty($params["tipo"]) || !in_array($params["tipo"], ["bebida", "comida"]) 
+        || !isset($params["nombre"]) || empty($params["nombre"]))
+    {
+        $response->getBody()->write(json_encode(["error" => "Completar todos los campos [tipo](bebida/comida)[nombre]"]));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    else
+    {
+        $db = conectar();
+        
+        if (productoExiste($params['tipo'], $params['nombre'], $db)) {
+            $response->getBody()->write(json_encode(["error" => "El producto ya existe en la base de datos"]));
+            return $response->withHeader('Content-Type', 'application/json');
+        }
+
+        $consulta = "INSERT INTO productos (tipo, nombre) VALUES (:tipo, :nombre)";
+
+        try
+        {
+            $insert = $db->prepare($consulta);
+            $insert->bindParam(':tipo', $params['tipo']);
+            $insert->bindParam(':nombre', $params['nombre']);
+            
+            $insert->execute();
+            $response->getBody()->write(json_encode(["mensaje" => "Producto agregado exitosamente"]));
+        }
+        catch (PDOException $exepcion)
+        {
+            $error = array("error" => $exepcion->getMessage());
+            $response->getBody()->write(json_encode($error));
+        }
+
+        return $response->withHeader('Content-Type', 'application/json');
+    }
 });
-*/
+
+$app->post("/mesas", function ($request, $response, array $args) {
+
+    // Obtener los datos del cuerpo de la solicitud
+    $mesasMaximas = 10;
+
+    $data = $request->getParsedBody();
+
+    if (!isset($data["max_comensales"]) || empty($data["max_comensales"]))
+    {
+        $response->getBody()->write(json_encode(["error" => "Completar la cantidad de max comensales [max_comensales]"]));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    else
+    {
+        $db = conectar();
+
+        if (mesasCompletas($db, $mesasMaximas)) {
+            $response->getBody()->write(json_encode(["error" => "No se pueden agregar más mesas, se ha alcanzado el límite máximo"]));
+            return $response->withHeader('Content-Type', 'application/json');
+        }
+
+        $consulta = "INSERT INTO mesas (max_comensales) VALUES (:max_comensales)";
+
+        try
+        {
+            $stmt = $db->prepare($consulta);
+            $stmt->bindParam(':max_comensales', $data['max_comensales']);
+            $stmt->execute();
+            $response->getBody()->write(json_encode(["mensaje" => "Mesa agregada exitosamente"]));
+        }
+        catch (PDOException $e)
+        {
+            $error = array("error" => $e->getMessage());
+            $response->getBody()->write(json_encode($error));
+        }
+
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+});
 
 /*
 http://localhost:666/
--- db/conectarDB.php
+++ db/conectarDB.php
@@ -19,4 +19,37 @@ function conectar()
         return null;
     }
 }
+function usuarioExiste($usuario, $db)
+{
+    $consulta = "SELECT COUNT(*) FROM usuarios WHERE usuario = :usuario";
+    $insert = $db->prepare($consulta);
+    $insert->bindParam(':usuario', $usuario);
+    $insert->execute();
+    
+    return $insert->fetchColumn() > 0;
+}
+
+
+function productoExiste($tipo, $nombre, $db)
+{
+    $consulta = "SELECT COUNT(*) FROM productos WHERE tipo = :tipo AND nombre = :nombre";
+    $insert = $db->prepare($consulta);
+    $insert->bindParam(':tipo', $tipo);
+    $insert->bindParam(':nombre', $nombre);
+    $insert->execute();
+    
+    return $insert->fetchColumn() > 0;
+}
+
+function mesasCompletas($db, $mesasMaximas)
+{
+    $consulta = "SELECT COUNT(*) FROM mesas";
+    $insert = $db->prepare($consulta);
+    $insert->execute();
+    
+    $cantidadMesas = $insert->fetchColumn();
+
+    return $cantidadMesas >= $mesasMaximas;
+}
+
 ?>
