b466bbf69367270fa053de0f57aeb0dc3e3a7bdc
Matias Skenen <93952537+matiastaoskn@users.noreply.github.com>
2024-06-09 11:46:23 -0300

    inserto base de datos y insert datos

--- app/index.php
+++ app/index.php
@@ -14,13 +14,57 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 
 $app->get("/usuarios", function(Request $request, Response $response, $args)
 {
+    $params = $request->getParsedBody();
+    
+    if (!isset($params['usuario']) || empty($params["usuario"]) || !isset($params['tipo']) || empty($params["tipo"]) || !isset($params['nombre']) || empty($params["nombre"]) || !isset($params['apellido']) || empty($params["apellido"]))
+    {
+        $response->getBody()->write(json_encode(["error" => "Debes completar todos los campos"]));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    else
+    {
+        $db = conectar();
+
+        $consulta = "INSERT INTO usuarios (usuario, tipo, nombre, apellido) 
+        VALUES (:usuario, :tipo, :nombre, :apellido)";
+    
+        try
+        {
+            $insert = $db->prepare($consulta);
+            $insert->bindParam(':usuario', $params['usuario']);
+            $insert->bindParam(':tipo', $params['tipo']);
+            $insert->bindParam(':nombre', $params['nombre']);
+            $insert->bindParam(':apellido', $params['apellido']);
+            
+            $insert->execute();
+            $response->getBody()->write(json_encode(["mensaje" => "Usuario agregado exitosamente"]));
+
+        }
+        catch (PDOException $exepcion)
+        {
+            $error = array("error" => $exepcion->getMessage());
+            $response->getBody()->write(json_encode($error));
+        }
+    
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    
+});
+
+/*
+$app->get("/usuarios", function(Request $request, Response $response, $args)
+{
     $params = $request->getQueryParams();
     
     $response->getBody()->write(json_encode($params)); 
     return $response;
 });
 
+
+
 ///http://localhost:666/
+//php -S localhost:666 -t app
 
 $app->run();
+*/
 ?>
---
+++ db/conectarDB.php
@@ -0,0 +1,22 @@
+<?php
+function conectar()
+{
+    try
+    {
+        $host = 'localhost';
+        $dbname = 'api-comanda-tp';
+        $username = 'root';
+        $password = '';
+
+        $conectar = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $conexion;
+    }
+    catch (PDOException $exepcion)
+    {
+        echo "Erro al conectar: " . $exepcion->getMessage() . "<br>";
+        return null;
+    }
+}
+?>
